8e3c62be01ba5116dbe7c989bdcbed00ded28883
AjitTiwari <ajittiwari0408@gmail.com>
2023-01-04 01:17:41 +0530

    Commited anout branches

--- LearnGIt.js
+++ LearnGIt.js
@@ -14,6 +14,7 @@ use >>git add -A to add all files from untracked to staging
 use  >>git comit --m "First commit"  to commit file from statging to main branch
 if any changes occurs in the file then it moves in modified zoneand needs to be staged and commited again
 
+use >>git log -p -2 to check the last 2 commit changes
 use >>git checkout "filename.txt"(for one file) || (--git checkout -f (for all file))   to make the file as previous committed file if any changes were made to file by user externally
 use >>git diff to compare your in working directory code with  staging code
 use >>git diff --staged to compare you staging code with last commit
@@ -26,5 +27,13 @@ use >>git rm --cached "filename.txt" to remoeve file from staging and puts to un
 use >>git status -s to check the file in which states using symbol
 
 .gitignore file is created to store information where we don't want to push to server
+
+--             Branhes to GIt
+
+use >>git branch -b feature1 to create a branch of name feature1 and switch in that branch
+use >>git branch  feature1 to create a branch of name feature1 and remain in master branch
+use >>git branch to see number of branches
+use >>git checkout feauture1 to switch our branch to featuure1
+use >>git merge feature1 to merge the branches
 
 */
