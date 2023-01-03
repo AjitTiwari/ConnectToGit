d400f5b8366d5c8cd9e1a7d412046b3f309b284f
AjitTiwari <ajittiwari0408@gmail.com>
2023-01-04 00:22:25 +0530

    File with git Steps

---
+++ LearnGIt.js
@@ -0,0 +1,14 @@
+// use git config --global user.name to find the name in bash
+// use git config --global user.email to find the name in bash
+
+// There are four regions
+
+//1. Untracted region
+//2. Unmodified region
+//3. modified region
+//4. Staged region
+
+// use --git status to find the status
+// use --git add -A to add all files from untracked to staging
+//use  --git comit --m "First commit"  to commit file from statging to main branch
+// if any changes occurs in the file then it moves in modified zoneand needs to be staged and commited again
